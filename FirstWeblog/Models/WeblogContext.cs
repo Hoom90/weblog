1647b99a1fccc25883442b5ac8e8c8d7085450da
Hoom <mohammad.mahdavi0015@gmail.com>
2021-12-10 11:53:03 +0330

    Initial commit

---
+++ FirstWeblog/Models/WeblogContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWeblog.Models
+{
+    public class WeblogContext : DbContext
+    {
+
+        DbSet<Member> Members { get; set; }
+        DbSet<Log> Logs { get; set; }
+    }
+}
---
+++ FirstWeblog/Models/WeblogDB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWeblog.Models
+{
+    public class Member
+    {
+        public int MemberID { get; set; }
+        public string Name { get; set; }
+
+        public List<Log> Logs { get; set; }
+
+    }
+    public class Log
+    {
+        public int LogID { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+
+
+        public Member Member { get; set; }
+    }
+}
